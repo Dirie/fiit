39684b7db82eccffe773c81a29ac1f7c3f2a1d54
abdulrazak.dirie@gmail.com <Dirie>
2019-06-18 01:04:24 +0300

    new project were added!

---
+++ fiit3/index.js
@@ -0,0 +1,79 @@
+const Joi = require("@hapi/joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello wolrd!!");
+});
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { error } = validate_Course(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id == parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the courses with the given id was not found");
+  res.send(course);
+});
+
+app.get("/api/courses/:year/:month", (req, res) => {
+  res.send(req.params + " " + req.query);
+  //res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the course you entered was not found.");
+
+  const { error } = validate_Course(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+validate_Course = course => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+};
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the course you entered was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
